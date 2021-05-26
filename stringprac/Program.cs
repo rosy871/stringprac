e77c3bd2939c93b6d3b5a5110212c16e92e5d819
rosy871 <yalameju@gmail.com>
2021-05-26 10:21:17 +0200

    Add project files.

---
+++ stringprac/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace stringprac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "this is test";
+            string str2 = "this is text";
+
+            if (String.Compare(str1,str2)==0)
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are equal.");
+            }
+            else
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are not equal.");
+            }
+            Console.ReadKey();
+
+            if (str1.Contains("test"))
+            {
+                Console.WriteLine("'test' word was found in str1.\n\n");
+            }
+            Console.ReadKey();
+
+            string[] sarray = new string[]
+            {
+                "my husband name is sudeep",
+                "my name is rosy",
+                "I have two children",
+                "my first child is boy",
+                "and his name is suroz",
+                "and my second child is girl",
+                "and her name is surina"
+            };
+            string str = String.Join("\n", sarray);
+            Console.WriteLine(str);
+
+        }
+    }
+}
